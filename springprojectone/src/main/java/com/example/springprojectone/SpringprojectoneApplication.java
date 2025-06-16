84b4b025e11f37bebea9627cbe99ad29a4f0b926
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-06-16 09:40:54 -0500

    initial commit

---
+++ springprojectone/src/main/java/com/example/springprojectone/SpringprojectoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectoneApplication.class, args);
+	}
+
+}
---
+++ springprojectone/src/test/java/com/example/springprojectone/SpringprojectoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
